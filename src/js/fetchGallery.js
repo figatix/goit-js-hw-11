c393d56b35b18e2861ed2fe9293771b84d7b2d58
figatix <60873339+figatix@users.noreply.github.com>
2023-01-28 08:17:36 +0200

    Update fetchGallery.js

--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 
 export default class PhotoAPI {
   
@@ -7,29 +9,57 @@ export default class PhotoAPI {
   constructor() {
     this.page = null;
     this.query = null;
-    this.per_page = 12;
+    this.per_page = 18;
   }
 
   fetchQuery() {
-    const searchOptions = new URLSearchParams ({
-      key: PhotoAPI.USER_KEY,
-      q: this.query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: this.per_page,
-      page: this.page
-    })
-
-    return fetch(`${PhotoAPI.BASE_URL}?${searchOptions}`).then(response =>{
-      if(!response.ok){
-        throw new Error(response.status)
+    const searchOptions = {
+      params: {
+        key: PhotoAPI.USER_KEY,
+        q: this.query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: this.per_page,
+        page: this.page
       }
-      return response.json()
-    })
-  }
+    }
 
+    return axios.get(`${PhotoAPI.BASE_URL}`, searchOptions)
+  }
 }
+
+// export default class PhotoAPI {
+  
+//   static USER_KEY = '33045436-8e58141e6d6ddbbbde7b75c75';
+//   static BASE_URL = 'https://pixabay.com/api/';
+
+//   constructor() {
+//     this.page = null;
+//     this.query = null;
+//     this.per_page = 12;
+//   }
+
+//   fetchQuery() {
+//     const searchOptions = new URLSearchParams ({
+//       key: PhotoAPI.USER_KEY,
+//       q: this.query,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//       per_page: this.per_page,
+//       page: this.page
+//     })
+
+//     return fetch(`${PhotoAPI.BASE_URL}?${searchOptions}`).then(response =>{
+//       if(!response.ok){
+//         throw new Error(response.status)
+//       }
+//       return response.json()
+//     })
+//   }
+
+// }
 
 
 
