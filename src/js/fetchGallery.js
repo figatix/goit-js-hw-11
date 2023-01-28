6ab1d80bb02b1d7070564336a339767048887bc1
figatix <60873339+figatix@users.noreply.github.com>
2023-01-28 20:57:39 +0200

    Update fetchGallery.js

--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 export default class PhotoAPI {
   
   static USER_KEY = '33045436-8e58141e6d6ddbbbde7b75c75';
@@ -28,45 +27,4 @@ export default class PhotoAPI {
     return axios.get(`${PhotoAPI.BASE_URL}`, searchOptions)
   }
 }
-
-// export default class PhotoAPI {
-  
-//   static USER_KEY = '33045436-8e58141e6d6ddbbbde7b75c75';
-//   static BASE_URL = 'https://pixabay.com/api/';
-
-//   constructor() {
-//     this.page = null;
-//     this.query = null;
-//     this.per_page = 12;
-//   }
-
-//   fetchQuery() {
-//     const searchOptions = new URLSearchParams ({
-//       key: PhotoAPI.USER_KEY,
-//       q: this.query,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//       per_page: this.per_page,
-//       page: this.page
-//     })
-
-//     return fetch(`${PhotoAPI.BASE_URL}?${searchOptions}`).then(response =>{
-//       if(!response.ok){
-//         throw new Error(response.status)
-//       }
-//       return response.json()
-//     })
-//   }
-
-// }
-
-
-
-
-
-
-
-
-
 
